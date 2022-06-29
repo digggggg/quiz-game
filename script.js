8ac5ffa63a43f2684b5dc007af778872bb159368
Jondo <chet9880@gmail.com>
2022-06-28 23:21:40 -0400

    added second question

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 
 
 loadPage();
-var secondsLeft = 101;
-var score = 0;
+var secondsLeft = 10;
 
 
 function loadPage(){
@@ -42,8 +41,10 @@ function gameTime(){
         secondsLeft--;
         timerlocation.innerHTML = secondsLeft
     
-        if (secondsLeft === 0){
+        if (secondsLeft <= 0){
             clearInterval(timerInterval)
+            document.body.style.display="none"
+            alert("You are out of time")
         }
     }, 1000);
     
@@ -58,6 +59,10 @@ function clearLoad(){
 function clearLoad2(){
     this.parentNode.style.display='none';
     loadSecondQuestion();
+}
+
+function clearLoad3(){
+    this.parentNode.style.display='none';
 }
 
 function loadFirstQuestion(){
@@ -107,29 +112,97 @@ function loadFirstQuestion(){
 
     document.body.appendChild(loadDiv)
     
-    
-
-    
-    
     document.getElementById('submit').addEventListener("click", function(){
 
-        if (document.getElementById("q1").checked == true){
-            score++;
-            return score;
-            
-        }
         if (document.getElementById("q2").checked == true){
             secondsLeft -= 10;
-            return score;
+            
         }
         
     })
 
     document.getElementById('submit').addEventListener("click", clearLoad2)
-    return score;
 }
 
 function loadSecondQuestion(){
+    var loadDiv = document.createElement("div")
+    loadDiv.setAttribute("class", "question-one")
+
+    var loadHead = document.createElement("h2")
+    loadHead.innerHTML = "Question 2"
+
+    var loadQuestion = document.createElement("p")
+    loadQuestion.innerHTML = "What method is used to set attributes for elements?"
+
+    var loadForm = document.createElement("form")
+
+    var loadLabel = document.createElement("label")
+    loadLabel.setAttribute("for", "q3")
+    loadLabel.innerHTML = "setAttribute"
+
+    var loadInput = document.createElement("input")
+    loadInput.setAttribute("id", "q3")
+    loadInput.setAttribute("type", "radio")
+    loadInput.setAttribute("name", "question1")
+
+    var loadLabel2 = document.createElement("label")
+    loadLabel2.setAttribute("for", "q4")
+    loadLabel2.innerHTML = "createElement"
+
+    var loadInput2 = document.createElement("input")
+    loadInput2.setAttribute("id", "q4")
+    loadInput2.setAttribute("type", "radio")
+    loadInput2.setAttribute("name", "question1")
+
+    var submitBtn = document.createElement("button")
+    submitBtn.setAttribute("type", "submit")
+    submitBtn.setAttribute("id", "submit1")
+    submitBtn.innerHTML = "Submit"
+
+    loadDiv.appendChild(loadHead)
+    loadDiv.appendChild(loadQuestion)
+    loadDiv.appendChild(loadForm)
+    loadDiv.appendChild(loadLabel)
+    loadDiv.appendChild(loadInput)
+    loadDiv.appendChild(loadLabel2)
+    loadDiv.appendChild(loadInput2)
+    loadDiv.appendChild(submitBtn)
+
+    document.body.appendChild(loadDiv)
+
+    document.getElementById('submit1').addEventListener("click", function(){
+            
+        if (document.getElementById("q4").checked == true){
+            secondsLeft -= 10;
+        }
+        
+    })
+
+    document.getElementById('submit1').addEventListener("click", clearLoad3)
+}
+
+function highscore(){
+    var loadDiv = document.createElement("div")
+    loadDiv.setAttribute("class", "highscore")
+
+    var loadForm = document.createElement("form")
+    var loadLabel = document.createElement("label")
+    loadLabel.setAttribute("for", "hs")
+    var loadInput = document.createElement("input")
+    loadInput.setAttribute("type", "text")
+    loadInput.setAttribute("id", "hs")
+
+    var submitBtn = document.createElement("button")
+    submitBtn.setAttribute("type", "submit")
+    submitBtn.setAttribute("id", "submit1")
+    submitBtn.innerHTML = "Submit"
+
+    loadDiv.appendChild(loadForm)
+    loadDiv.appendChild(loadLabel)
+    loadDiv.appendChild(loadInput)
+    loadDiv.appendChild(submitBtn)
+
+    document.body.appendChild(loadDiv)
     
 }
 
