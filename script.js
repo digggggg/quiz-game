3784572fffb4409eb3c690284dbaced892b0883e
Jondo <chet9880@gmail.com>
2022-06-28 20:40:56 -0400

    added dynamically generated first question

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 loadPage();
-
+var score;
 
 
 function loadPage(){
@@ -29,9 +29,62 @@ function loadPage(){
 
 function clearLoad(){
     this.parentNode.style.display='none';
+    loadFirstQuestion();
 }
 
+function clearLoad2(){
+    this.parentNode.style.display='none';
+    loadSecondQuestion();
+}
 
+function loadFirstQuestion(){
+    var loadDiv = document.createElement("div")
+    loadDiv.setAttribute("class", "question-one")
+
+    var loadHead = document.createElement("h2")
+    loadHead.innerHTML = "Question 1"
+
+    var loadQuestion = document.createElement("p")
+    loadQuestion.innerHTML = "What notation is used commonly when formatting javascript?"
+
+    var loadForm = document.createElement("form")
+
+    var loadLabel = document.createElement("label")
+    loadLabel.setAttribute("for", "q1")
+    loadLabel.innerHTML = "Dot Notation"
+
+    var loadInput = document.createElement("input")
+    loadInput.setAttribute("id", "q1")
+    loadInput.setAttribute("type", "radio")
+
+    var loadLabel2 = document.createElement("label")
+    loadLabel2.setAttribute("for", "q2")
+    loadLabel2.innerHTML = "Scientific Notation"
+
+    var loadInput2 = document.createElement("input")
+    loadInput2.setAttribute("id", "q2")
+    loadInput2.setAttribute("type", "radio")
+
+    loadDiv.appendChild(loadHead)
+    loadDiv.appendChild(loadQuestion)
+    loadDiv.appendChild(loadForm)
+    loadDiv.appendChild(loadLabel)
+    loadDiv.appendChild(loadInput)
+
+    document.body.appendChild(loadDiv)
+    
+    if (document.getElementById("loadInput").checked == true){
+        score++;
+    }
+
+    clearLoad2();
+
+    return score;
+}
+
+function loadSecondQuestion(){
+    
+}
 
 
 
